ba4c733b918c5576286df0c87e8a6cf796b58177
StefanThadeus <61950524+StefanThadeus@users.noreply.github.com>
2023-09-16 15:55:55 +0200

    Introduce cookies and sessions to the app
    Introduce cookies and sessions to the app via the passport package, implement logout

--- app.js
+++ app.js
@@ -1,85 +1,120 @@
 //jshint esversion:6
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const saltRounds = 10;
+const session = require("express-session");
+const passportLocalMongoose = require("passport-local-mongoose");
+const passport = require("passport");
 
 const app = express();
 
 app.use(express.static("public"));
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.set("view engine", "ejs");
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
-mongoose.connect("mongodb://127.0.0.1:27017/userDB", {useNewUrlParser: true});
+app.use(
+  session({
+    secret: "Our little secret.",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
 });
+
+userSchema.plugin(passportLocalMongoose);
 
 const User = new mongoose.model("User", userSchema);
 
-app.get("/", function(req, res) {
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.get("/", function (req, res) {
   res.render("home");
 });
 
-app.get("/login", function(req, res) {
+app.get("/login", function (req, res) {
   res.render("login");
 });
 
-app.get("/register", function(req, res) {
+app.get("/register", function (req, res) {
   res.render("register");
 });
 
-app.post("/register", function(req, res) {
-  bcrypt.hash(req.body.password, saltRounds, function(error, hash) {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash
-    });
+app.get("/secrets", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("secrets");
+  } else {
+    res.redirect("/login");
+  }
+});
 
-    try {
-      saveNewUser(newUser).then(function() {
-          res.render("secrets");
+app.get("/logout", function (req, res) {
+  req.logout(function (err) {
+    if (err) {
+      console.log(err);
+    }
+    res.redirect("/");
+  });
+});
+
+app.post("/register", function (req, res) {
+  User.register(
+    { username: req.body.username },
+    req.body.password,
+    function (err, user) {
+      if (err) {
+        console.log(err);
+        res.redirect("/register");
+      } else {
+        passport.authenticate("local")(req, res, function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  );
+});
+
+app.post("/login", function (req, res) {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+  });
+
+  req.login(user, function (err) {
+    if (err) {
+      console.log(err);
+    } else {
+      passport.authenticate("local")(req, res, function () {
+        res.redirect("/secrets");
       });
-    } catch (error) {
-      console.log(error);
     }
   });
 });
 
-app.post("/login", function(req, res) {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  try {
-    findUserByUsername(username).then(function(foundUser) {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, function(error, result) {
-          if (result === true) {
-            res.render("secrets");
-          }
-        });
-      }
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-const saveNewUser = async function(newUser) {
+const saveNewUser = async function (newUser) {
   return await newUser.save();
-}
+};
 
-const findUserByUsername = async function(username) {
-  return await User.findOne({email: username});
-}
+const findUserByUsername = async function (username) {
+  return await User.findOne({ email: username });
+};
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000.");
 });
